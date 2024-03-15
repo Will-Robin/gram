1ef2415451ac6bc580a32d9f059f3323256aa020
William E. Robinson <werobinson6@gmail.com>
2024-03-15 17:34:38 +0100

    update rdkit based run reaction function

--- gram/chemoinformatics/core/rdkit/run_reaction.py
+++ gram/chemoinformatics/core/rdkit/run_reaction.py
@@ -38,18 +38,25 @@ def run_rdkit_reaction(
     product_sets = reaction_rule.RunReactants(reactant_compounds)
 
     for product_set in product_sets:
+        valid_products = []
         for product in product_set:
             # The products are checked for chiral information which has been
             # transferred to achiral carbons, which is removed.
             product = incorrect_chiral_H_solve(product)
             if sanitize:
-                Chem.SanitizeMol(product)
+                try:
+                    Chem.SanitizeMol(product)
+                    valid_products.append(product)
+                except:
+                    pass
+            else:
+                valid_products.append(product)
 
         rxn = AllChem.ChemicalReaction()  # Create an empty chemical reaction
         [rxn.AddReactantTemplate(r) for r in reactant_compounds]
         [rxn.AddProductTemplate(p) for p in product_set]
 
-        reaction_SMILES = AllChem.ReactionToSmiles(rxn)
+        reaction_SMILES = AllChem.ReactionToSmiles(rxn, canonical=True)
 
         reactions.append(reaction_SMILES)
 
